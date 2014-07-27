a7e48a4bccc18088be4c47ba1632a44a8d8b3923
Atsushi Odagiri <aodagx@gmail.com>
2014-07-28 01:04:23 +0900

    added helper api bound locale

--- example/index.mako
+++ example/index.mako
@@ -4,10 +4,10 @@ ${panel('breadcrumb', breadcrumb=layout.page_context.breadcrumb)}
 ${message}
 ${request.locale_name}
 <p>
-${h.format_datetime(now, format="full", locale=request.locale_name)}
+${h.format_datetime(now, format="full")}
 <p>
 ${now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}
 <p>
-${h.format_datetime(utcnow, format="full", locale=request.locale_name)}
+${h.format_datetime(utcnow, format="full")}
 <p>
 ${utcnow.strftime("%Y-%m-%d %H:%M:%S %Z%z")}
--- rebecca/bootstraplayout/helpers.py
+++ rebecca/bootstraplayout/helpers.py
@@ -1,3 +1,23 @@
+import functools
 from webhelpers2.html import HTML, escape, literal
 from babel.dates import format_date, format_datetime, format_time
 from babel.numbers import format_number, format_decimal, format_percent
+
+
+def bind_locale(func, localename):
+    return functools.partial(func, locale=localename)
+
+
+class WebHelper(object):
+    def __init__(self, request):
+        self.request = request
+        self.locale_name = request.locale_name
+        self.HTML = HTML
+        self.escape = escape
+        self.literal = literal
+        self.format_date = bind_locale(format_date, self.locale_name)
+        self.format_datetime = bind_locale(format_datetime, self.locale_name)
+        self.format_time = bind_locale(format_time, self.locale_name)
+        self.format_number = bind_locale(format_number, self.locale_name)
+        self.format_decimal = bind_locale(format_decimal, self.locale_name)
+        self.format_percent = bind_locale(format_percent, self.locale_name)
